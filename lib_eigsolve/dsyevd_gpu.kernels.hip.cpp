4f4b3051ef7736f0373b52fea1f667099429e8df
Yuhui Bao <julie.bao32@gmail.com>
2020-09-07 11:45:30 -0400

    fix problem size Y launch_krnl_b1f342_1_auto

--- lib_eigsolve/dsyevd_gpu.kernels.hip.cpp
+++ lib_eigsolve/dsyevd_gpu.kernels.hip.cpp
@@ -222,7 +222,7 @@ extern "C" void launch_krnl_b1f342_1_auto(const int sharedMem,
                                           const int v_lb2,
                                           int k) {
   const unsigned int krnl_b1f342_1_NX = k;
-  const unsigned int krnl_b1f342_1_NY = n;
+  const unsigned int krnl_b1f342_1_NY = k;
 
   const unsigned int krnl_b1f342_1_blockX = 16;
   const unsigned int krnl_b1f342_1_blockY = 16;
